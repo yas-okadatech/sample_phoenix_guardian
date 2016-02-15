79105c2c8d8cb576253e545c4b502a24f25db9fb
Yas Okada <yas.okadatech@gmail.com>
2016-02-16 08:08:54 +0900

    ueberauth : auth controller

--- web/controllers/api/v1/session_controller.ex
+++ web/controllers/api/v1/session_controller.ex
@@ -1,6 +1,8 @@
 defmodule SamplePhoenixReactApp.Api.V1.SessionController do
   use SamplePhoenixReactApp.Web, :controller
 
+  plug Ueberauth
+
   require Logger
 
   alias SamplePhoenixReactApp.UserAuth
@@ -62,5 +64,39 @@ defmodule SamplePhoenixReactApp.Api.V1.SessionController do
     conn
     |> put_status(403)
     |> json(%{ error: :forbidden })
+  end
+
+
+  alias Ueberauth.Strategy.Helpers
+
+  def request(conn, _params) do
+    render(conn, "request.html", callback_url: Helpers.callback_url(conn))
+  end
+
+  def delete(conn, _params) do
+    conn
+    |> put_flash(:info, "You have been logged out!")
+    |> configure_session(drop: true)
+    |> redirect(to: "/")
+  end
+
+  def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
+    conn
+    |> put_flash(:error, "Failed to authenticate.")
+    |> redirect(to: "/")
+  end
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    case UserFromAuth.find_or_create(auth) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Successfully authenticated.")
+        |> put_session(:current_user, user)
+        |> redirect(to: "/")
+      {:error, reason} ->
+        conn
+        |> put_flash(:error, reason)
+        |> redirect(to: "/")
+    end
   end
 end
